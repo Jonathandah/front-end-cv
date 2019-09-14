03ed1ef03f3dc02f45f8a2d8aedb8d35023d316c
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-09-14 21:48:31 +0200

    styled header with color and logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Other from "./components/Other";
 import MaterialIcon, { colorPalette } from "material-icons-react";
+import logo from "./undraw_online_cv_qy9w.svg";
 
 function App() {
   return (
@@ -15,12 +16,8 @@ function App() {
       />
       <div className="App">
         <header className="App__header">
-          <h1 className="App__header__title">Jonathan Dahnberg</h1>
-          <MaterialIcon
-            icon="accessibility_new"
-            size={500}
-            color="white"
-          ></MaterialIcon>
+          <p className="App__header__title">Jonathan Dahnberg</p>
+          <img className="App__header__logo" src={logo} />
         </header>
         <main className="App__main">
           <Route exact path="/" component={Home} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 function Home() {
   return (
     <div className="Home">
-      <header className="Home__header"></header>
       <main className="Home__main"></main>
     </div>
   );
